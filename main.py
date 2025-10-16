9b966c9eb1c3d9508885a32fc5f5f636121f26ae
Maria Fernanda Cenci <mariafcenci@gmail.com>
2025-10-16 12:04:45 +0200

    Added menu title and options

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from package.Game import Game
 game = Game()
 print('rodou1')
 game.run()
-print('rodou2')
+
 
--- package/Menu.py
+++ package/Menu.py
@@ -1,5 +1,8 @@
 import pygame.image
 from pygame import Surface, Rect
+from pygame.font import Font
+
+from package.Constants import WINDOW_WIDTH
 
 
 class Menu:
@@ -14,6 +17,10 @@ class Menu:
         pygame.mixer_music.play(-1) # in loop
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(60, 'Water', (255, 178, 102),(390, 145), 'Impact')
+            self.menu_text(60, 'Shoot', (255, 178, 102), (538, 145), 'Impact')
+            self.menu_text(35, 'START', (255, 255, 255), ((WINDOW_WIDTH / 2), 240), 'Consolas')
+            self.menu_text(35, 'QUIT', (255, 255, 255), ((WINDOW_WIDTH / 2), 290), 'Consolas')
             pygame.display.flip()
             # print('before handle events')
             for event in pygame.event.get():
@@ -22,7 +29,51 @@ class Menu:
                     print('quit')
                     quit() # end init pygame
 
-            # print('after handle events')
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_position: tuple, text_font: str):
+        text_font: Font = pygame.font.SysFont(name=text_font, size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_position)
+        self.window.blit(source=text_surf, dest=text_rect)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
